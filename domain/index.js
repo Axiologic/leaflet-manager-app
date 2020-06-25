85cd8470cc27837f9e526fcf99f7673bc47659a4
skutner <sava.dumitru.daniel@gmail.com>
2020-06-25 16:58:05 +0300

    added dossierBuilder swarm

--- domain/index.js
+++ domain/index.js
@@ -1,1 +1,36 @@
 //Add specific code here (swarms, flows, assets, transactions)
+$$.swarm.describe("dossierBuilder", {
+    createLeafletDossier: function (leaflet) {
+        require("edfs").attachWithSeed(rawDossier.getSeed(), (err, edfs) => {
+            console.log("Got edfs instance");
+            if (err) {
+                return this.return(err);
+            }
+            edfs.createRawDossier((err, dossier) => {
+                if (err) {
+                    return this.return(err);
+                }
+                console.log("created raw dossier");
+                console.log("About to read attachment", leaflet.attachment);
+                rawDossier.readFile(leaflet.attachment, (err, attachmentData) => {
+                    if (err) {
+                        console.log(err);
+                        return this.return(err);
+                    }
+
+                    console.log("Read attachment data");
+                    dossier.writeFile("/attachment.pdf", attachmentData, (err) => {
+                        if (err) {
+                            return this.return(err);
+                        }
+                        console.log("wrote attachment to dossier");
+                        dossier.writeFile("/leaflet.json", JSON.stringify(leaflet), (err) => {
+                            console.log("Wrote leaflet.json");
+                            this.return(err, dossier.getSeed());
+                        });
+                    });
+                });
+            });
+        });
+    }
+});
