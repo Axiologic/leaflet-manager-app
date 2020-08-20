0f556a33a71f861443df99db7ddc0edc4f650a10
skutner <sava.dumitru.daniel@gmail.com>
2020-08-20 15:18:00 +0300

    updated dossierBuilder swarm to use new edfs APi

--- domain/index.js
+++ domain/index.js
@@ -1,71 +1,92 @@
 //Add specific code here (swarms, flows, assets, transactions)
+function initializeBDNS(callback) {
+    rawDossier.getKeySSI((err, keySSI) => {
+        if (err) {
+            return callback(err);
+        }
+
+        const keySSIInstance = require("key-ssi-resolver").KeySSIFactory.create(keySSI);
+        $$.BDNS.addConfig("default", {
+            endpoints: [
+                {
+                    endpoint: keySSIInstance.getHint(),
+                    type: 'brickStorage'
+                },
+                {
+                    endpoint: keySSIInstance.getHint(),
+                    type: 'anchorService'
+                }
+            ]
+        })
+        callback(undefined);
+    });
+}
+
 $$.swarm.describe("dossierBuilder", {
     createLeafletDossier: function (leaflet) {
-        require("edfs").attachWithSeed(rawDossier.getSeed(), (err, edfs) => {
+        const EDFS = require("edfs");
+        initializeBDNS((err) => {
             if (err) {
                 return this.return(err);
             }
-            edfs.createRawDossier((err, dossier) => {
+
+            EDFS.createDSU("RawDossier", (err, dossier) => {
                 if (err) {
                     return this.return(err);
                 }
-                console.log("created raw dossier");
-                console.log("About to read attachment", leaflet.attachment);
+
                 rawDossier.readFile(leaflet.attachment, (err, attachmentData) => {
                     if (err) {
                         console.log(err);
                         return this.return(err);
                     }
 
-                    console.log("Read attachment data", attachmentData);
                     dossier.writeFile("/attachment.pdf", attachmentData, (err) => {
                         if (err) {
                             return this.return(err);
                         }
-                        console.log("wrote attachment to dossier");
+
                         dossier.writeFile("/leaflet.json", JSON.stringify(leaflet), (err) => {
                             if (err) {
                                 return this.return(err);
                             }
-                            console.log("Wrote leaflet.json");
+
                             rawDossier.listMountedDossiers("/", (err, mounts) => {
-                                if(err){
+                                if (err) {
                                     return this.return(err);
                                 }
+
                                 let constitutionSeed = mounts[0].identifier;
                                 dossier.mount("/code", constitutionSeed, (err) => {
-                                    this.return(err, dossier.getSeed());
+                                    if (err) {
+                                        return this.return(err);
+                                    }
+
+                                    dossier.getKeySSI((err, keySSI) => {
+                                        this.return(err, keySSI);
+                                    });
                                 });
                             });
-
                         });
                     });
                 });
             });
         });
     },
-    createMessageDSU: function (message, leafletDSUSeed) {
-        require("edfs").attachWithSeed(rawDossier.getSeed(), (err, edfs) => {
-            console.log("Got edfs instance");
+    createMessageDSU: function (message, leafletDSUKeySSI) {
+        const EDFS = require("edfs");
+        EDFS.createDSU("RawDossier", (err, dossier) => {
             if (err) {
                 return this.return(err);
             }
-            edfs.createRawDossier((err, dossier) => {
+
+            dossier.writeFile("/message.json", JSON.stringify(message), (err) => {
                 if (err) {
                     return this.return(err);
                 }
-                console.log("created raw dossier");
 
-                dossier.writeFile("/message.json", JSON.stringify(message), (err) => {
-                    console.log("Wrote leaflet.json");
-                    if (err) {
-                        return this.return(err);
-                    }
-
-                    dossier.mount("/leaflet", leafletDSUSeed, (err) => {
-                        console.log("Mounted leaflet DSU");
-                        this.return(err, dossier.getSeed());
-                    });
+                dossier.mount("/leaflet", leafletDSUKeySSI, (err) => {
+                    this.return(err, dossier.getSeed());
                 });
             });
         });
